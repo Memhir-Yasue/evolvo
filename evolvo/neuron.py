21beffa0c80f86f36e8ff351b6fb02683851833c
Memhir-Yasue <eyasu.woldu@temple.edu>
2022-01-29 17:52:57 -0500

    update neuron class

--- evolvo/neuron.py
+++ evolvo/neuron.py
@@ -1,17 +1,20 @@
 class Neuron:
 
-    def __init__(self, id: int, value: float):
+    def __init__(self, id: int, value: float = None):
         self.id = id
         self.value = value
+        self.connections_count = 0
         self.connections = []
 
-    def connect_with(self, id: int):
-        self.connections.append(id)
-        return f"Connected with {id}"
+    def connect_with(self, neuron_id: int):
+        self.connections.append(neuron_id)
+        self.connections_count += 1
+        return f"Connected with {neuron_id}"
 
     def disconnect(self, id: int):
         if id in self.connections:
             self.connections.remove(id)
+            self.connections_count -= 1
             return f"Disconnected from {id}"
 
     def get_connections(self):
